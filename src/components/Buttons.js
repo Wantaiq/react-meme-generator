cff217a9fa36658bd55e9de3255369239e7c8ddf
Filip <filipdopa@yahoo.com>
2022-05-11 20:55:26 +0200

    Fix download button layout

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,15 +1,13 @@
 export default function Button(props) {
   return (
     <div className="buttons flex-col">
-      <div className="flex-row">
-        <button
-          data-test-id="generate-meme"
-          onClick={props.handleTemplateSubmit}
-          htmlFor="generate-template"
-        >
-          Generate meme
-        </button>
-      </div>
+      <button
+        data-test-id="generate-meme"
+        onClick={props.handleTemplateSubmit}
+        htmlFor="generate-template"
+      >
+        Generate meme
+      </button>
       <button
         onClick={props.handleFileDownload}
         className="download-btn"
