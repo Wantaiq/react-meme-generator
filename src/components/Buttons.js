520e88b5a61f81fcc4220b31af9685928bb71c09
Filip <filipdopa@yahoo.com>
2022-05-11 16:55:15 +0200

    Create initial layout

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,17 +1,21 @@
 export default function Button(props) {
   return (
-    <>
-      <button onClick={props.handlePreview} data-test-id="generate-meme">
-        See preview of your meme
+    <div className="buttons flex-col">
+      <div className="flex-row">
+        <button
+          data-test-id="generate-meme"
+          onClick={props.handleTemplateSubmit}
+          htmlFor="generate-template"
+        >
+          Generate template
+        </button>
+        <button onClick={props.handlePreview} data-test-id="generate-meme">
+          Preview meme
+        </button>
+      </div>
+      <button className="download-btn" htmlFor="download-btn">
+        Download
       </button>
-      <button
-        data-test-id="generate-meme"
-        onClick={props.handleTemplateSubmit}
-        htmlFor="generate-template"
-      >
-        Generate template
-      </button>
-      <button htmlFor="download-btn"> Download </button>
-    </>
+    </div>
   );
 }
